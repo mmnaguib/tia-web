d8367719556c33ba45ee17a81eb4fadfb73c569f
Mohammed Naguib <mnaguib126@gmail.com>
2024-08-19 15:47:51 +0300

    edit products category

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,9 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import actProductsCategory from "../../app/act/Product/actProductsCategory";
-import { IProduct } from "../../interfaces";
 import { UseAppDispatch } from "../../app/hooks";
 import Axiosinstance from "../../config/axiosInstanse";
-import ProductList from "../Products/ProductList";
 
 export interface ICategory {
   name: string;
@@ -12,7 +10,6 @@ export interface ICategory {
 
 const Categories = () => {
   const [categories, setCategories] = useState<ICategory[]>([]);
-  const [categoryProducts, setCategoryProducts] = useState<IProduct[]>([]);
   const dispatch = UseAppDispatch();
 
   const fetchCategories = async () => {
@@ -31,10 +28,6 @@ const Categories = () => {
   const categoryProductsHandler = useCallback(
     async (categoryName: string) => {
       try {
-        const res = await Axiosinstance.get<IProduct[]>(
-          `/catProducts?categoryName=${categoryName}`
-        );
-        setCategoryProducts(res.data);
         dispatch(actProductsCategory(categoryName));
       } catch (error) {
         console.error("Error fetching category products:", error);
